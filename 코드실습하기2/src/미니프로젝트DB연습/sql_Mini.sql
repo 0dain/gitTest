bd681226b26ee1895a8cfd8633d3a786677f0f51
임다인 <@192.168.25.39>
2022-09-04 16:02:32 +0900

    회원정보 테이블 PK 이름 바꿈

--- 코드실습하기2/src/미니프로젝트DB연습/sql_Mini.sql
+++ 코드실습하기2/src/미니프로젝트DB연습/sql_Mini.sql
@@ -1,7 +1,7 @@
 /* 가제: 갓생살기 테이블*/
  /* 회원정보 테이블 */
 create table Minimember(mem_id varchar2(10), mem_pw varchar2(20) not null,
-constraint mem_id_pk primary key(mem_id)
+constraint minimem_memid_pk primary key(mem_id)
 );
 
 /* 캐릭터 정보 테이블 -> 3개까지만 만들 수 있음, 캐릭터 타입 중복 가능*/
