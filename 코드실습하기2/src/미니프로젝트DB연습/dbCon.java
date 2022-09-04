8ddf6d18442f38e367570e04eee4267d58cebf4d
임다인 <@192.168.25.39>
2022-09-04 11:44:17 +0900

    디비컨드롤러 생성

---
+++ 코드실습하기2/src/미니프로젝트DB연습/dbCon.java
@@ -0,0 +1,15 @@
+package 미니프로젝트DB연습;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class dbCon {
+
+	//객체 생성
+			Connection conn=null;
+			PreparedStatement psmt=null;
+			ResultSet rs=null;
+	
+	
+}
