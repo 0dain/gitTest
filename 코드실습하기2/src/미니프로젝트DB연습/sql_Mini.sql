6d17190d5a53b3a9b4e052ae940c6e97bf37ce9a
임다인 <@192.168.25.39>
2022-09-04 15:43:20 +0900

    sql 연결1

--- 코드실습하기2/src/미니프로젝트DB연습/sql_Mini.sql
+++ 코드실습하기2/src/미니프로젝트DB연습/sql_Mini.sql
@@ -4,6 +4,6 @@ create table godlifemember(mem_id varchar2(10), mem_pw varchar2(20) not null,
 constraint mem_id_pk primary key(mem_id)
 );
 
-/* 캐릭터 정보 테이블*/
+/* 캐릭터 정보 테이블 -> 3개까지만 만들 수 있음, 캐릭터 타입 중복 가능*/
 create table charac(ch_nick varchar2(10), cha_type 
 );
