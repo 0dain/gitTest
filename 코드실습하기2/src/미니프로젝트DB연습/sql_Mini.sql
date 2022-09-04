fa86f4f2f56349bc7440241d4e85b04f39fb7d7f
임다인 <@192.168.25.39>
2022-09-04 15:47:30 +0900

    회원정보 테이블명 변경

--- 코드실습하기2/src/미니프로젝트DB연습/sql_Mini.sql
+++ 코드실습하기2/src/미니프로젝트DB연습/sql_Mini.sql
@@ -1,6 +1,6 @@
 /* 가제: 갓생살기 테이블*/
  /* 회원정보 테이블 */
-create table godlifemember(mem_id varchar2(10), mem_pw varchar2(20) not null,
+create table Minimember(mem_id varchar2(10), mem_pw varchar2(20) not null,
 constraint mem_id_pk primary key(mem_id)
 );
 
